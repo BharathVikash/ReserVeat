121fc67dfe2e71f611a34dac2868f4581f9f2436
Bharath Vikash <betabharath12404@gmail.com>
2024-08-27 20:15:25 +0530

    Initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Box,Typography} from '@mui/material'
+import './footer.css'
+import {Link} from 'react-router-dom'
+const Footer = () => {
+  return (
+    <Box className='footer'>
+        <Typography component='p'>
+            </Typography>
+    </Box>
+  )
+}
+
+export default Footer
